e1ba52aa9c6d5de56f057e50c60a2ffdfb1f8622
Jaewon <woin2ee@gmail.com>
2022-06-23 19:00:23 +0900

    Coordinator pattern 을 사용하지 않고 view 이동 구현

--- CoordinatorPatternExample/Presentation/HomeScene/View/HomeViewController.swift
+++ CoordinatorPatternExample/Presentation/HomeScene/View/HomeViewController.swift
@@ -12,4 +12,22 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func moveFirstViewButtonDidTap(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "First", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: String(describing: FirstViewController.self))
+        present(vc, animated: true)
+    }
+    
+    @IBAction func moveSecondViewButtonDidTap(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Second", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: String(describing: SecondViewController.self))
+        present(vc, animated: true)
+    }
+    
+    @IBAction func moveThirdViewButtonDidTap(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Third", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: String(describing: ThirdViewController.self))
+        present(vc, animated: true)
+    }
 }
