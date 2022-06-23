74101130497103c5269c36f8b9ee1b44c74df585
Jaewon <woin2ee@gmail.com>
2022-06-23 18:09:35 +0900

    프로젝트 초기 설정

--- CoordinatorPatternExample/Presentation/HomeScene/View/HomeViewController.swift
+++ CoordinatorPatternExample/Presentation/HomeScene/View/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  CoordinatorPatternExample
 //
 //  Created by Jaewon on 2022/06/23.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class HomeViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
